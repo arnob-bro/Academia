2f0425fc68612c7d487d2e99b7b487907ae67cac
Sayeb Mohaimen <muhaimen360@gmail.com>
2025-03-02 03:45:30 +0600

    Course Schedule Page(Admin) Added

--- Academia_Frontend/src/App.jsx
+++ Academia_Frontend/src/App.jsx
@@ -29,6 +29,7 @@ import FacultyAttendanceTracker from "./components/facultyPages/facultyAttendanc
 import FacultyLeave from "./components/facultyPages/facultyLeaveApplication/facultyLeave.jsx";
 import FacultyLeaveAdmin from "./components/adminPages/facultyLeaveAdmin/facultyLeaveAdmin.jsx"; 
 import CourseManagement from "./components/adminPages/courseManagement/courseManagement.jsx"; 
+import CourseSchedule from "./components/adminPages/courseScheduleManagement/courseSchedule.jsx";
 
 function App() {
   const userData = JSON.parse(localStorage.getItem("userData"));
@@ -40,7 +41,6 @@ function App() {
         <Route path="/reset-password" element={<ResetPassword />} /> 
         
         
-
         {!userData ? (<><Route path="*" element={<Navigate to="/login" replace />}/> 
             
           </>
@@ -92,7 +92,8 @@ function App() {
                 <Route path="/Student-Admission" element={<StudentAdmission />} />
                 <Route path="/faculty-management" element={<FacultyManagement />} />
                 <Route path="/faculty-recruitment" element={<FacultyRecruitment />} /> 
-                <Route path="/course-management-admin" element={<CourseManagement/>}/>
+                <Route path="/course-management-admin" element={<CourseManagement/>}/> 
+                <Route path="/course-schedule" element={<CourseSchedule/>}/>
               </>
             )}
           </>
