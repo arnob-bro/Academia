ecadc2397a97b4328e5c3f3b20d8dcca7c8d3791
mubassera <mubasseramahdi@gmail.com>
2025-03-02 06:49:53 +0600

    connecting backend to frontend for attendance part

--- Academia_Frontend/src/Api/faculty.js
+++ Academia_Frontend/src/Api/faculty.js
@@ -1,3 +1,19 @@
 import axios from "axios";
 
 const baseURL = "http://127.0.0.1:8000/api/faculty";
+
+export const handleFetchCoursesOfAFacultyApi = async (facultyID) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await axios.get(`${baseURL}/my-courses/`, facultyID, config);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching courses:", error);
+    throw error;
+  }
+};
