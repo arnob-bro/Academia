21e558a92ba0a46746f4257e0390e5060078ba1d
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-03-05 06:41:04 +0600

    solved route issues

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -46,6 +46,7 @@ Route::get('/faculty/courses/{courseID}/all-students', [FacultyController::class
 Route::post('/faculty/schedule/student-attendance/',[FacultyController::class, 'postAttendanceStatusOfStudents']);
 Route::get('/faculty/schedule/student-attendance/weeks', [FacultyController::class, 'getAllWeeksForAttendanceHistory']);
 Route::post('/faculty/assessment/assessment-creation', [FacultyController::class, 'CreateAssessment']);
+Route::get('/faculty/courses/assessments', [FacultyController::class, 'getAssessmentsByCourseAndSemester']);
 
 
 Route::post('/student/AllInformationsOfStudent', [StudentController::class, 'storeAllInformationsOfStudent']);
